c7038d549fb1bc6366ad969f353ec0230b2ae521
Uthmanbello <uthmanbello88@gmail.com>
2023-09-29 17:20:56 +0100

    Style upload image div

--- src/components/PictureUploadForm.js
+++ src/components/PictureUploadForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import uploadIcon from '../images/upload-icon.svg';
 
 function PictureUploadForm() {
   const [selectedFile, setSelectedFile] = useState(null);
@@ -34,45 +35,57 @@ function PictureUploadForm() {
 //   };
 
   return (
-    <div>
-      <input
-        type="file"
-        id="fileInput"
-        accept="image/*"
-        onChange={handleFileChange}
-        style={{ display: 'none' }}
-      />
-      {selectedFile && !editing && (
-        <div>
-          <img
-            src={URL.createObjectURL(selectedFile)}
-            alt="Selected"
-            width={150}
-            height={150}
+    <div className='form-div'>
+      <div className='title'>
+        <p>Upload cover image</p>
+      </div>
+      <div className='upload-div'>
+        <div className='column btn-div'>
+          <img src={uploadIcon} alt="upload icon" className="upload-icon" onClick={() => document.getElementById('fileInput')?.click()} />
+          <input
+            type="file"
+            id="fileInput"
+            accept="image/*"
+            onChange={handleFileChange}
+            style={{ display: 'none' }}
           />
-          {/* <button onClick={handleEdit}>Edit</button>
-          <button onClick={handleDelete}>Delete</button> */}
+          {selectedFile && !editing && (
+            <div>
+              <img
+                src={URL.createObjectURL(selectedFile)}
+                alt="Selected"
+                width={150}
+                height={150}
+              />
+              {/* <button onClick={handleEdit}>Edit</button>
+              <button onClick={handleDelete}>Delete</button> */}
+            </div>
+          )}
+          {selectedFile && editing && (
+            <div>
+              <img
+                src={URL.createObjectURL(selectedFile)}
+                alt="Selected"
+                width={150}
+                height={150}
+              />
+              <button onClick={handleUpload}>Upload</button>
+              <button onClick={handleDelete}>Cancel</button>
+            </div>
+          )}
+          <div>      
+            {!selectedFile && ( 
+              <input
+                type="button"
+                value="Upload cover image"
+                onClick={() => document.getElementById('fileInput')?.click()}
+                className='upload-btn'
+              />
+            )}
+          </div>
+          <p>16:9 ratio is the recommended. Max image size 1mb </p>
         </div>
-      )}
-      {selectedFile && editing && (
-        <div>
-          <img
-            src={URL.createObjectURL(selectedFile)}
-            alt="Selected"
-            width={150}
-            height={150}
-          />
-          <button onClick={handleUpload}>Upload</button>
-          <button onClick={handleDelete}>Cancel</button>
-        </div>
-      )}
-      {!selectedFile && (
-        <input
-          type="button"
-          value="Upload Photo"
-          onClick={() => document.getElementById('fileInput')?.click()}
-        />
-      )}
+      </div>
     </div>
   );
 }
