1f9a3c39d2ab89689db966a88c416323d14e34c0
Uthmanbello <uthmanbello88@gmail.com>
2023-09-29 13:53:52 +0100

    Add basic top bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar';
+import Mainpage from './pages/Mainpage';
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Mainpage />
     </div>
   );
 }
---
+++ src/pages/Mainpage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/Mainpage.css';
+
+function Mainpage() {
+  return (
+    <div className="mainpage">
+      <ul className='top-bar'>
+        <li>Program Details</li>
+        <li>Application form</li>
+        <li>Workflow</li>
+        <li>Preview</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Mainpage;
