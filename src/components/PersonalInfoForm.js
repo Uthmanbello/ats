a2ebe956c15e95ebbadd666c296f7358f1329656
Uthmanbello <uthmanbello88@gmail.com>
2023-09-29 18:10:29 +0100

    style check buttons position

--- src/components/PersonalInfoForm.js
+++ src/components/PersonalInfoForm.js
@@ -14,8 +14,20 @@ function PersonalInfoForm() {
         <input type="text" id="lastName" name="lastName" className='info-input'/>
         <label For="firstName" className='info-label'>Email</label>
         <input type="email" id="email" name="email" className='info-input'/>
-        <label For="phone" className='info-label'>Phone <span>(without dial code)</span></label>
-        <input type="email" id="email" name="email" className='info-input'/>
+        <div>
+          <div className='row check-btns'>
+            <span className='align-center'>
+              <input type="checkbox" id="phone-internal" name="phone-internal" className=''/>
+              <label For="phone-internal" className='checkbox-label'>Internal</label>
+            </span>
+            <span className='align-center'>
+              <input type="checkbox" id="phone-hide" name="phone-hide" className=''/>
+              <label For="phone-hide" className='checkbox-label'>Hide</label>
+            </span>
+          </div>
+          <label For="phone" className='info-label'>Phone <span>(without dial code)</span></label>
+          <input type="email" id="email" name="email" className='info-input'/>
+        </div>
       </div>
     </div>
   );
